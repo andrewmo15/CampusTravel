a5bd0e363af993ad822d77e227b4a2b9fc96542b
Andrew Mo <andrewmoasdf@gmail.com>
2020-08-23 17:20:39 -0400

    first commit

---
+++ CampusTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampusTravel
+//
+//  Created by Andrew Mo on 8/23/20.
+//  Copyright © 2020 Andrew Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
